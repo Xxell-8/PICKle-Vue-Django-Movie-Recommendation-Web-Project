309b63b8e41c3d138a8bca6791adac6d8dd01cd3
Xxell-8 <cell8pepper@gmail.com>
2021-05-18 18:15:59 +0900

    ADD skeleton codes

---
+++ server/accounts/urls.py
@@ -0,0 +1,10 @@
+from rest_framework_jwt.views import obtain_jwt_token
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('signup/', views.signup),
+    path('api-token-auth/', obtain_jwt_token),
+]
