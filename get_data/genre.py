8422a7a832543a19e84305945fcaa3df14534dfa
Xxell-8 <cell8pepper@gmail.com>
2021-05-20 23:53:12 +0900

    ADD movie & genre data

---
+++ get_data/genre.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from pprint import pprint
+
+API_KEY = 'fe7f6fee767abe126d012ca0b0f22fca'
+URL = f'https://api.themoviedb.org/3/genre/movie/list?api_key={API_KEY}&language=ko-KR'
+
+genre_json = requests.get(URL).json()
+
+genres = []
+
+for genre in genre_json['genres']:
+    genre_data = {}
+    genre_data['model'] = 'movies.genre'
+    genre_data['pk'] = genre.get('id')
+    genre_data['fileds'] = {
+        'name': genre.get('name')
+    }
+    genres.append(genre_data)
+
+#pprint(genres)
+
+with open('tmdb.json', 'a', encoding='utf-8') as f:
+    json.dump(genres, f, ensure_ascii=False, indent='\t')
