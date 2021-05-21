ce13c4efd63e5319c4aae9aebf659e2cd221ef72
unknown <aidenrism@gmail.com>
2021-05-21 12:04:39 +0900

    FIX movie_list&detail

--- server/movies/views.py
+++ server/movies/views.py
@@ -1,4 +1,3 @@
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404, get_list_or_404
@@ -8,20 +7,13 @@ from .serializers import MovieSerializer
 
 
     
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def movie_list(request):
-    if request.method == 'GET':
-        movies = get_list_or_404(Movie)
-        serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data)
+    movies = get_list_or_404(Movie)
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
     
-    elif request.method == 'POST':
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
- 
- 
+
 @api_view(['GET'])
 def movie_detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
