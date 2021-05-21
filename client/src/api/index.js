0b1e0288e94d7341beef48dc1d368193596e166d
Xxell-8 <cell8pepper@gmail.com>
2021-05-21 11:10:12 +0900

    temp

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const SERVER_URL = process.env.VUE_APP_SERVER_URL
+const YOUTUBE_API_URL = process.env.VUE_APP_YOUTUBE_API_URL
+const YOUTUBE_API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
 
 export default {
   // auth
@@ -10,4 +12,16 @@ export default {
   signup(userData) {
     return axios.post(`${SERVER_URL}/accounts/signup/`, userData)
   },
+  getVideo(movieTitle) {
+    return axios({
+      url: YOUTUBE_API_URL,
+      method: 'get',
+      params: {
+        key: YOUTUBE_API_KEY,
+        part: 'snippet',
+        type: 'video',
+        q: movieTitle + 'trailer'
+      }
+    })
+  }
 }
