dd4b8e8da62baff156973070e6dfa57feff8e36a
unknown <aidenrism@gmail.com>
2021-05-21 10:27:31 +0900

    ADD movies serializer

--- server/movies/serializers.py
+++ server/movies/serializers.py
@@ -1,1 +1,11 @@
 from rest_framework import serializers
+from .models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    comments = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
