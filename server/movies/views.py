e58d8d05115a80542a7799c4209aab72312cc984
unknown <aidenrism@gmail.com>
2021-05-21 11:51:09 +0900

    ADD movie_list movie_detail

--- server/movies/views.py
+++ server/movies/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Movie
+from .serializers import MovieSerializer
+# from server.movies import serializers
 
-# Create your views here.
+
+    
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = get_list_or_404(Movie)
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+ 
+ 
+@api_view(['GET'])
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
