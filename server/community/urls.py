6a21e9f1126d370a3e887976f2e7fb46b746b60a
unknown <aidenrism@gmail.com>
2021-05-22 15:59:23 +0900

    ADD curation list&detail view

--- server/community/urls.py
+++ server/community/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'community'
 
 urlpatterns = [
-   
+    path('', views.curation_list),
+    path('<int:curation_pk>', views.curation_detail),
 ]
--- server/community/views.py
+++ server/community/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404, get_list_or_404
+from .models import Curation
+# from django.contrib.auth import get_user_model
+from .serializers import CurationSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def curation_list(request):
+    curations = get_list_or_404(Curation)
+    serializer = CurationSerializer(curations, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def curation_detail(request, curation_pk):
+    curation = get_object_or_404(Curation, pk=curation_pk)
+    serializer = CurationSerializer(curation)
+    return Response(serializer.data)
