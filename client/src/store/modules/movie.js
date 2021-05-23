0fd14c000d99341826229fe30a6330765677427f
Xxell-8 <cell8pepper@gmail.com>
2021-05-23 15:14:41 +0900

    UPDATE vuex

--- client/src/store/modules/movie.js
+++ client/src/store/modules/movie.js
@@ -2,7 +2,12 @@ import api from '@/api'
 // import router from '@/router'
 
 const state = {
-  movies: []
+  movies: [],
+  bestMovies: [],
+  genreOptions: [],
+  searchResult: null,
+  recommendOptions: ['random', 'genre', 'similar', 'series', 'classic'],
+  picked: false
 }
 
 const actions = {
@@ -12,13 +17,49 @@ const actions = {
       console.log(response)
       commit('SET_MOVIES', response.data)
     }
-  }
+  },
+  async getBestMovies({ commit }) {
+    const response = await api.getBestMovies()
+    if (response.status === 200) {
+      console.log(response)
+      commit('SET_BEST_MOVIES', response.data)
+    }
+  },
+  async getGenreList({ commit }) {
+    const response = await api.getGenreList()
+    if (response.status === 200) {
+      commit('SET_GENRE_OPTIONS', response.data)
+    }
+  },
+  async searchData ({ commit }, inputText) {
+    if (inputText.length >= 1) {
+      const response = await api.searchData(inputText)
+      if (response.status === 200) {
+        console.log(response)
+        commit('SET_SEARCH_RESULTS', response.data)
+      }
+    } else {
+      commit('RESET_SEARCH_RESULTS')
+    }
+  },
 }
 
 const mutations = {
   SET_MOVIES(state, payload) {
     state.movies = payload
-  }
+  },
+  SET_BEST_MOVIES(state, payload) {
+    state.bestMovies = payload
+  },
+  SET_GENRE_OPTIONS(state, payload) {
+    state.genreOptions = payload
+  },
+  SET_SEARCH_RESULTS(state, payload) {
+    state.searchResult = payload
+  },
+  RESET_SEARCH_RESULTS(state) {
+    state.searchResult = []
+  }, 
 }
 
 const getters = {
