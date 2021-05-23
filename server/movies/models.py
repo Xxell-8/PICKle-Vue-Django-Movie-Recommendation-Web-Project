371c83bd0dfd3616343afb5cec888798c92b27bd
Xxell-8 <cell8pepper@gmail.com>
2021-05-23 15:17:20 +0900

    UPDATE movies server

--- server/movies/models.py
+++ server/movies/models.py
@@ -5,6 +5,9 @@ from django.conf import settings
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     genres = models.ManyToManyField(Genre)
@@ -15,10 +18,9 @@ class Movie(models.Model):
     
     title = models.CharField(max_length=50)
     overview = models.TextField()
-    poster_path = models.CharField(max_length=500)
-    backdrop_path = models.CharField(max_length=500, null=True)
+    poster_path = models.CharField(max_length=200)
+    backdrop_path = models.CharField(max_length=200, null=True)
     release_date = models.DateField()
-    # 0보다 작은 정수값 허용 x 
     director = models.CharField(max_length=50)
     runtime = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
@@ -29,10 +31,8 @@ class Movie(models.Model):
 
 
 class Comment(models.Model):
-
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
--- server/movies/views.py
+++ server/movies/views.py
@@ -1,14 +1,20 @@
-# from typing_extensions import runtime
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.shortcuts import get_object_or_404, get_list_or_404
-from .models import Movie, Genre
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
+from django.db.models import Count
 from django.contrib.auth import get_user_model
-from .serializers import MovieSerializer
-from django.db.models import Q, Count
-# from server.movies import serializers
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+from .models import Movie, Genre
+from .serializers import MovieSerializer, GenreListSerializer
 
 
+User = get_user_model()
+
+# 1. Default - 전체 영화 데이터
 @api_view(['GET'])
 def movie_list(request):
     movies = get_list_or_404(Movie)
@@ -16,6 +22,7 @@ def movie_list(request):
     return Response(serializer.data)
 
 
+# 1. Default - 단일 영화 상세 데이터
 @api_view(['GET'])
 def movie_detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
@@ -23,53 +30,86 @@ def movie_detail(request, movie_pk):
     return Response(serializer.data)
 
 
+# 1. Default - 장르 데이터
 @api_view(['GET'])
-def short_movie_list(request):
-    # movies = get_list_or_404(Movie)
-    # runtime = request.GET.get('runtime', None)
-    # Movie.objects.exclude(runtime__isnull=True)
-    # if runtime:
-    # if Movie.objects.exclude(runtime__isnull=False):
-    # Entry.objects.filter(pub_date__isnull=False).latest('pub_date')
-    movies = Movie.objects.order_by('runtime').filter()
-    # else:
-    #     movies = Movie.objects.exclude(runtime__isnull=True)
-    serializer = MovieSerializer(movies, many=True)
+def genre_list(request):
+    genres = get_list_or_404(Genre)
+    serializer = GenreListSerializer(genres, many=True)
     return Response(serializer.data)
 
 
-@api_view(['GET'])
-def random_recommended(request):
-    movies = Movie.objects.order_by("?")[:5]
-    # movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+# 1. Default - PICK / WISH / WATCH / DISLIKE
+@api_view(['POST'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
+def rating(request, movie_pk, options):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+
+    if options == 'pick':
+        if movie.pick_users.filter(pk=request.user.pk).exists():
+            movie.pick_users.remove(request.user)
+        else:
+            movie.pick_users.add(request.user)
+
+    elif options == 'wish':
+        if movie.wish_users.filter(pk=request.user.pk).exists():
+            movie.wish_users.remove(request.user)
+        else:
+            movie.wish_users.add(request.user)
+
+    elif options == 'watch':
+        if movie.watch_users.filter(pk=request.user.pk).exists():
+            movie.watch_users.remove(request.user)
+        else:
+            movie.watch_users.add(request.user)
+
+    elif options == 'dislike':
+        if movie.dislike_users.filter(pk=request.user.pk).exists():
+            movie.dislike_users.remove(request.user)
+        else:
+            movie.dislike_users.add(request.user)
+
+    return Response(status=status.HTTP_200_OK)
 
 
-@api_view(['GET'])
-def genre_recommended(request):
-    my_genre = get_user_model.objects.get('genre')
-    movies = Movie.objects.filter(genre=my_genre).order_by("?")[:5]
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
-
-
-
+# 2. HOME > PICK Best
 @api_view(['GET'])
 def pick_best(request):
-
-    movies = Movie.objects.all().annotate(pick_nums=Count('pick_users'))[:5]
-    # movies = Movie.objects.order_by('-pick_users')[:10]
-    # movies = Movie.objects.filter(pick_user__gte=0)
+    movies = Movie.objects.annotate(pick_count=Count('pick_users')).order_by('-pick_count')[:10]
     serializer = MovieSerializer(movies, many =True)
     return Response(serializer.data)
 
 
+# 3. Search > 영화 검색 데이터 
+@api_view(['GET'])
+def movie_search_list(request):
+    target = request.GET.get('q')
+    results = Movie.objects.filter(title__icontains=target)
+    serializer = MovieSerializer(results, many=True)
+    return Response(serializer.data)
+
+
+# 4. Recommend > 랜덤 영화 추천 
+@api_view(['GET'])
+def random_movie_list(request):
+    movies = Movie.objects.order_by("?")[:5]
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+
+
+# 6.
+# @api_view(['GET'])
+# def short_movie_list(request):
+#     movies = Movie.objects.order_by('runtime')[:5]
+#     serializer = MovieSerializer(movies, many=True)
+#     return Response(serializer.data)
+
 
 
 # @api_view(['GET'])
-# def search(request):
-#     text = request.GET.get('q')
-#     movies = Movie.objects.filter(title__icontains=text)
-#     serializer = MovieSerializer(movies, many =True)
+# def genre_recommended(request):
+#     my_genre = get_user_model.objects.get('genre')
+#     movies = Movie.objects.filter(genre=my_genre).order_by("?")[:5]
+#     serializer = MovieSerializer(movies, many=True)
 #     return Response(serializer.data)
