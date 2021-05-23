66e5cbe1c32a1409e6ae1ff53f206e0d51c28786
Xxell-8 <cell8pepper@gmail.com>
2021-05-23 15:16:25 +0900

    UPDATE accounts model

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from movies.models import Genre
 
 
 class User(AbstractUser):
-    pass
+    genres = models.ManyToManyField(Genre)
